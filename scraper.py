4cea94109435f633d453cd58e333b1b667b653a3
mmihnea05 <mosucmihnea112@gmail.com>
2026-07-14 23:15:49 +0300

    add Libertatea category

--- scraper.py
+++ scraper.py
@@ -182,6 +182,18 @@ def find_category(soup, url):
         path_parts = parsed_url.path.strip('/').split('/')
         return path_parts[0].replace('-', ' ').capitalize()
     
+    # libertatea
+    if 'libertatea.ro' in url:
+        breadcrumbs = soup.find('span', class_='breadcrumbs')
+        if breadcrumbs:
+            links = breadcrumbs.find_all('a')
+            if len(links) >= 3:
+                # home | stiri | categorie
+                # links[-1] extrage ultimul element care este categoria
+                return links[-1].get_text(strip=True)
+            elif len(links) >= 2:
+                return links[-1].get_text(strip=True)
+    
     return 'General'
 
 def find_published_date(soup):
@@ -286,8 +298,8 @@ def get_links_from_file(file_path):
 # exemplu utilizare
 #scrape_article("https://stirileprotv.ro/stiri/international/politico-o-ancheta-de-corup-ie-a-declansat-remanierea-surprinzatoare-a-cabinetului-ucrainean-decisa-de-zelenski.html")
 #scrape_article("https://www.digi24.ro/stiri/externe/mapamond/donald-trump-sustine-ca-sua-ar-trebui-sa-controleze-stramtoarea-ormuz-si-ameninta-iranul-o-sa-i-lovim-foarte-tare-3860305")
-scrape_article("https://www.antena3.ro/sport/noua-tari-din-ue-inclusiv-romania-solicita-reducerea-finantarii-comitetului-olimpic-international-din-cauza-reprimirii-rusilor-795622.html")
-#scrape_article("https://www.libertatea.ro/stiri/stiri-brasov-fabrica-purolite-brasov-investeste-560000-euro-sistem-tratare-apa-5814473")
+#scrape_article("https://www.antena3.ro/sport/noua-tari-din-ue-inclusiv-romania-solicita-reducerea-finantarii-comitetului-olimpic-international-din-cauza-reprimirii-rusilor-795622.html")
+scrape_article("https://www.libertatea.ro/stiri/stiri-brasov-fabrica-purolite-brasov-investeste-560000-euro-sistem-tratare-apa-5814473")
 #scrape_article("https://hotnews.ro/sorin-grindeanu-virulent-la-adresa-pnl-usr-ne-vom-bate-cu-aceasta-pesta-a-hastagilor-pe-tot-terenul-2299618")
 #scrape_article("https://economedia.ro/info-sud-est-cum-a-ratat-delta-dunarii-proiecte-pnrr-in-valoare-de-50-de-milioane-de-euro.html")
 #scrape_article("https://profit.ro/povesti-cu-profit/energie/pas-inainte-dupa-esec-complexul-energetic-oltenia-si-alro-slatina-pas-inainte-pentru-baterii-de-950-mw-langa-fotovoltaicele-ceo-omv-petrom-tinmar-dupa-esecul-centralei-pe-gaze-naturale-22515087")
