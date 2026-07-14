cc28f049d2d7b12b7ec01085e23c561bd7a91461
mmihnea05 <mosucmihnea112@gmail.com>
2026-07-14 23:32:24 +0300

    add Economedia category

--- scraper.py
+++ scraper.py
@@ -200,6 +200,14 @@ def find_category(soup, url):
         if section_meta and section_meta.get('content'):
             return section_meta['content'].strip()
     
+    # economedia
+    if 'economedia.ro' in url:
+        breadcrumbs = soup.find('div', class_='single__breadcrumbs')
+        if breadcrumbs:
+            links = breadcrumbs.find_all('a')
+            if links:
+                return links[-1].get_text(strip=True)
+
     return 'General'
 
 def find_published_date(soup):
@@ -306,8 +314,8 @@ def get_links_from_file(file_path):
 #scrape_article("https://www.digi24.ro/stiri/externe/mapamond/donald-trump-sustine-ca-sua-ar-trebui-sa-controleze-stramtoarea-ormuz-si-ameninta-iranul-o-sa-i-lovim-foarte-tare-3860305")
 #scrape_article("https://www.antena3.ro/sport/noua-tari-din-ue-inclusiv-romania-solicita-reducerea-finantarii-comitetului-olimpic-international-din-cauza-reprimirii-rusilor-795622.html")
 #scrape_article("https://www.libertatea.ro/stiri/stiri-brasov-fabrica-purolite-brasov-investeste-560000-euro-sistem-tratare-apa-5814473")
-scrape_article("https://hotnews.ro/sorin-grindeanu-virulent-la-adresa-pnl-usr-ne-vom-bate-cu-aceasta-pesta-a-hastagilor-pe-tot-terenul-2299618")
-#scrape_article("https://economedia.ro/info-sud-est-cum-a-ratat-delta-dunarii-proiecte-pnrr-in-valoare-de-50-de-milioane-de-euro.html")
+#scrape_article("https://hotnews.ro/sorin-grindeanu-virulent-la-adresa-pnl-usr-ne-vom-bate-cu-aceasta-pesta-a-hastagilor-pe-tot-terenul-2299618")
+scrape_article("https://economedia.ro/mercedes-benz-anunta-ca-a-investit-un-miliard-de-euro-pentru-a-dubla-capacitatea-fabricii-din-kecskemet.html")
 #scrape_article("https://profit.ro/povesti-cu-profit/energie/pas-inainte-dupa-esec-complexul-energetic-oltenia-si-alro-slatina-pas-inainte-pentru-baterii-de-950-mw-langa-fotovoltaicele-ceo-omv-petrom-tinmar-dupa-esecul-centralei-pe-gaze-naturale-22515087")
 #scrape_article("https://www.zf.ro/carturesti-se-extinde-in-audio/mihaela-pana-post-merger-integration-manager-audiotribe-roman-marile-23190496")
 #scrape_article("https://www.mediafax.ro/politic/grindeanu-spune-ca-e-de-acord-cu-propunerile-facute-de-varujan-pambuccian-si-kelemen-hunor-privind-noul-guvern-23771576")
